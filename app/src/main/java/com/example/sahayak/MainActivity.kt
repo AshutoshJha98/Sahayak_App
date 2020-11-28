0753d02e2e7024432bec2eeced9cf7cd94e39f51
AshutoshJha98 <jashutosh0@gmail.com>
2020-11-28 17:19:24 +0530

    Can read external files now

--- app/src/main/java/com/example/sahayak/MainActivity.kt
+++ app/src/main/java/com/example/sahayak/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.sahayak
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
+import android.widget.TextView
+import java.io.*
+import java.lang.StringBuilder
+import java.nio.file.StandardOpenOption
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,15 @@ class MainActivity : AppCompatActivity() {
         val Exercise_Button = findViewById<Button>(R.id.Exercixe_Button)
         val Finance_Button = findViewById<Button>(R.id.Finance_Button)
         val Analysis_Button = findViewById<Button>(R.id.Analysis_Button)
+        val console = findViewById<TextView>(R.id.TextConsole)
+
+        /*val fileContent:String = "Test123 \n"
+        var myExternalFile:File = File(getExternalFilesDir(""),"test.txt")
+        val fileOutPutStream = FileOutputStream(myExternalFile,true)
+        fileOutPutStream.write(fileContent.toByteArray())
+        fileOutPutStream.close()*/
+
+
         //Button presses on main activity open the respective activities
         Diet_Button.setOnClickListener()
         {
@@ -38,6 +52,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,UnderDevelopment :: class.java)
             startActivity(intent)
         }
+
 
 
     }
